2a9bacd2d09b0e554a64b173378b943fd59d8e27
GunelMustafayeva21 <gunel.isa2000@gmail.com>
2024-09-02 14:10:52 +0400

    User1 pushed to his branch

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
     <>
       <div>
 
-      <h1>I am John</h1>
-      <p>I finished project</p>
+      <h1>I am User1</h1>
+      <p>I made changes here my mistake</p>
 
        
       </div>
