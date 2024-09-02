60c4496058df0a4f939bf986c2359c80d27b8c1c
GunelMustafayeva21 <gunel.isa2000@gmail.com>
2024-09-02 15:14:40 +0400

    Buttons added from stash

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ function App() {
       <h1>I am User2</h1>
       <p>I made changes. But it is correct version</p>
 
+      <button style={{backgroundColor:"blue"}}>View More...</button>
+      <button style={{backgroundColor:"red"}}>I don't care</button>
 
       </div>
       
