ec066ee3cc64ab4f8ce21c2c9f5c96b29a6ee47c
GunelMustafayeva21 <gunel.isa2000@gmail.com>
2024-09-03 12:25:08 +0400

    Green Button

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
       <h1>I am User2</h1>
       <p>I made changes. But it is correct version</p>
-       
+       <button style={{backgroundColor:'green'}}></button>
 
       </div>
       
